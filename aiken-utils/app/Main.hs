f0db5aa114c159bd7cbd37ea9cd8d6d101ec4aba
Juan Salvador Magán Valero <jmaganvalero@gmail.com>
2024-06-06 10:51:12 +0200

    refactor: akien-utils testing vectors

--- aiken-utils/app/Main.hs
+++ aiken-utils/app/Main.hs
@@ -1,7 +1,14 @@
 module Main where
 
-import Testing (printMembershipTestingVectors)
+import Testing (
+    printMembershipTestingVectors,
+    printNonMembershipTestingVectors,
+ )
 
 main :: IO ()
 main = do
-    printMembershipTestingVectors
+    putStrLn "use aiken_bilinear_accumulator/accumulator.{"
+    putStrLn "  check_membership, check_nonmembership,"
+    putStrLn "  }\n\n"
+    mapM_ printMembershipTestingVectors ([1, 2, 3, 4] <> [5, 10 .. 45])
+    mapM_ printNonMembershipTestingVectors ([1, 2, 3, 4] <> [5, 10 .. 45])
